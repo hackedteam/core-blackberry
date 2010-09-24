6ebed1b88b2b14b8510f4eaa679df321c7114b02
Fabrizio Cornelli <f.cornelli@hackingteam.it>
2010-09-24 12:50:48 +0000

    New logAction
    git-svn-id: http://rcs-dev/svn/rcs/RCSBlackBerry/Workspace_5/trunk@2917 30e694c7-159d-4bd8-ba75-215c342f8d20

---
+++ RCSBlackBerry/src/blackberry/action/LogAction.java
@@ -0,0 +1,54 @@
+package blackberry.action;
+
+import java.io.EOFException;
+
+import net.rim.device.api.util.DataBuffer;
+import blackberry.debug.Debug;
+import blackberry.debug.DebugLevel;
+import blackberry.event.Event;
+import blackberry.utils.WChar;
+
+public class LogAction extends SubAction {
+
+	//#ifdef DEBUG
+	static Debug debug = new Debug("LogAction", DebugLevel.VERBOSE);
+	//#endif
+
+	private String info;
+
+	public LogAction(int actionId) {
+		super(actionId);
+	}
+
+	public LogAction(final int actionId_, final byte[] confParams) {
+		super(actionId_);
+		parse(confParams);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see blackberry.action.SubAction#execute(blackberry.event.Event)
+	 */
+	public boolean execute(final Event triggeringEvent) {
+
+		Debug.logToInfo(info, DebugLevel.INFORMATION);
+		return true;
+	}
+
+	protected boolean parse(byte[] confParams) {
+		final DataBuffer databuffer = new DataBuffer(confParams, 0,
+				confParams.length, false);
+		try {
+			final int len = databuffer.readInt();
+			final byte[] buffer = new byte[len];
+			databuffer.read(buffer);
+			info = WChar.getString(buffer, true);
+
+		} catch (final EOFException e) {
+			return false;
+		}
+		
+		return true;
+	}
+
+}
