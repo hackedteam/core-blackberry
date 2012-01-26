dfb7706ff7fb3ea401511624924af0064d68ea3b
Fabrizio Cornelli <finance@suppose.it>
2012-01-26 10:05:07 +0100

    7.6 RC5

--- RCSBlackBerry/src/blackberry/config/Cfg.java
+++ RCSBlackBerry/src/blackberry/config/Cfg.java
@@ -2,8 +2,8 @@ package blackberry.config;
 
 public class Cfg {
     
-    public static final int BUILD_ID = 9;
-    public static final String BUILD_TIMESTAMP = "20120123-015351";
+    public static final int BUILD_ID = 10;
+    public static final String BUILD_TIMESTAMP = "20120126-100122";
     
     //==========================================================
     // Static configuration
